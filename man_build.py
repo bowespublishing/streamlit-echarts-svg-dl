e87ee557742c3a56aca80970a8d3970a13540941
cyber qin <hu578344563@hotmail.com>
2023-06-01 01:49:31 +0800

    1.add build script 2.bump ver to 0.5.4

---
+++ man_build.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+from pathlib import Path
+from prompt_toolkit import prompt,HTML
+from tomlkit import dumps, loads, document, comment,load,dump
+import json
+
+if __name__=="__main__":
+
+    current = Path(__file__).parent  
+    print(current.joinpath('pyproject.toml').exists())
+    with open(current.joinpath('pyproject.toml'),mode='r+',encoding='utf8')  as io:
+        py_toml = load(io)
+        current_ver = py_toml['project']['version']
+        new_ver = prompt(HTML(f'<ansigreen>current pkg version:{current_ver}，use current or input new one, format-major.minor.patch:</ansigreen>'))
+        if new_ver != '':            
+            py_toml['project']['version'] = new_ver
+            io.seek(0)
+            io.truncate()
+            dump(py_toml,io)
+            with open(current.joinpath('streamlit_echarts/frontend/package.json'),mode='r+',encoding='utf8') as js:
+                conf = json.load(js)
+                conf['version']=new_ver
+                js.seek(0)
+                js.truncate()
+                json.dump(conf,js,indent=2)                
+    
+    os.chdir(current.joinpath('streamlit_echarts/frontend'))
+    subprocess.run(['npm','run','build'])
+    os.chdir(current)
+    subprocess.run(['python','-m','build'])
